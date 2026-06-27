43b4bcfe5f484ff37413a6f536792d009b870c1e
semen4yar-ui <semen4.yar@gmail.com>
2026-06-28 00:02:15 +0300

    Update cypress.config.js

--- ClickUpCypressApi/cypress.config.js
+++ ClickUpCypressApi/cypress.config.js
@@ -1,8 +1,13 @@
 const { defineConfig } = require("cypress");
+require("dotenv").config();
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      config.env.baseUrl = process.env.BASE_URL;
+      config.env.token = process.env.CLICKUP_TOKEN;
+      config.env.teamId = process.env.TEAM_ID;
+
       return config;
     },
   },
